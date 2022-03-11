f86b0bde3d141a80b9a3496a30f6a596c4ca3a3d
mMichele-github <100371043+mMichele-github@users.noreply.github.com>
2022-03-11 18:32:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+  //Qui vanno definite le variabili
   float a=0, b=0, c=0;
-    cout << "Inserire 3 valori reali in metri" << endl;
+  cout << "Inserire 3 valori reali in metri" << endl;
 
-cin >> a >> b >> c;
+  cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-   cout << ((a*b)/2) << endl << (a*a) << endl << (a*b) << endl << ((a+b)*c)/2 << endl;
-   return 0;
+  //Qui va scritto il codice per eseguire i calcoli richiesti
+  cout << ((a*b)/2) << endl << (a*a) << endl << (a*b) << endl << ((a+b)*c)/2 << endl;
+  return 0;
 }
